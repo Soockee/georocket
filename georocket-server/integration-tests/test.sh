6e9178b854d02f8c62a8bf46c1c4bac9518c86ac
Simon Stockhause <simon@pc2012.igd.fraunhofer.de>
2020-02-21 14:13:11 +0100

    run tests async

--- georocket-server/integration-tests/test.sh
+++ georocket-server/integration-tests/test.sh
@@ -2,8 +2,25 @@
 
 set -e
 
-./test-one.sh standalone
-./test-one.sh h2
-./test-one.sh mongo
-./test-one.sh s3
-./test-one.sh hdfs
+# all possible options of test-one.sh
+declare -a array_test_commands
+array_test_commands[0]=standalone
+array_test_commands[1]=h2
+array_test_commands[2]=mongo
+array_test_commands[3]=s3
+array_test_commands[4]=hdfs
+
+# pids of lunched backgroundprocess
+declare -a pids
+
+
+# run processes and store pids in array
+for i in $array_test_commands; do
+    ./procs[${i}] &
+    pids[${i}]=$!
+done
+
+# wait for all pids
+for pid in ${pids[*]}; do
+    wait $pid
+done
